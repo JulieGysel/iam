80ce210db05e32435a28b9917ef5c3d418f88bab
JulieGysel <julie.gysel@gmail.com>
2021-02-13 00:03:43 +0100

    new approach

--- pr1/queens.py
+++ pr1/queens.py
@@ -3,56 +3,56 @@
 import sys
 import numpy as np
 
+QUEEN = 1
+BANNED = -1
 
-def checkcol(arr, pos, num):
-    while pos > 0:
-        if arr[pos] in queens:
-            return False
-        else:
-            pos -= num
-    return True
-
-
-def checkDia1(arr, pos, num):
-    while pos > 0:
-        if arr[pos] in queens:
-            return False
-        else:
-            pos -= (num-1)
-    return True
-
-
-def checkDia2(arr, pos, num):
-    while pos > 0:
-        if arr[pos] in queens:
-            return False
-        else:
-            pos -= (num+1)
-    return True
-
-
-def place(arr, row, num):
-    for i in range(row*num, row*num+num):
-        pos = row*num+i
-        if pos == 0:
-            continue
-        if checkcol(arr, pos, num) and checkDia1(arr, pos, num) and checkDia2(arr, pos, num):
-            queens.append(pos)
-            return
-
-
-# main
 num = int(sys.argv[1])
-
 print('p cnf', num*num, num)
-arr = np.array(list(range(1, num*num+1)))
-queens = []
-
-place(arr, 0, num)
-place(arr, 1, num)
+array = [[0 for i in range(num)] for i in range(num)]
 
 
-print(queens)
+def ban(arr, row, col):
+    arr[row] = [BANNED for i in range(num)]
+    col1 = col
+    col2 = col
 
-# for i in range(num):
-#     print(*arr[i*num:i*num + num], 0)
+    for i in range(row, num):
+        arr[i][col] = BANNED
+
+    for i in range(row, num):
+        if col1 < 0:
+            break
+        arr[i][col1] = BANNED
+        col1 -= 1
+
+    while row < num and col2 < num:
+        arr[row][col2] = BANNED
+        row += 1
+        col2 += 1
+
+    return arr
+
+
+def place(arr, row):
+    if row >= num:
+        return True
+    for i in range(len(arr[row])):
+        if arr[row][i] == 0:
+            arr = ban(arr, row, i)
+            arr[row][i] = QUEEN
+            return
+            # return place(arr, row+1)
+
+    print('problem', row)
+    return False
+
+
+place(array, 0)
+place(array, 1)
+place(array, 2)
+place(array, 3)
+
+end_list = list(range(1, num+1))
+print(end_list)
+for i in array:
+    print(*i, 0)
