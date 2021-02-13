8881bd1a9f6aee06a93878c43f8400fcefa0419a
JulieGysel <julie.gysel@gmail.com>
2021-02-13 17:59:33 +0100

    Working, but not finding the correct solution

--- pr1/queens.py
+++ pr1/queens.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import copy
 import numpy as np
 
 QUEEN = 1
@@ -9,50 +10,64 @@ BANNED = -1
 num = int(sys.argv[1])
 print('p cnf', num*num, num)
 array = [[0 for i in range(num)] for i in range(num)]
+array[0][0] = BANNED
+
+
+def print_arr(arr):
+    for j in arr:
+        print(*j)
 
 
 def ban(arr, row, col):
-    arr[row] = [BANNED for i in range(num)]
     col1 = col
     col2 = col
 
+    for i in range(num):
+        arr[row][i] = BANNED
     for i in range(row, num):
         arr[i][col] = BANNED
-
-    for i in range(row, num):
-        if col1 < 0:
-            break
-        arr[i][col1] = BANNED
-        col1 -= 1
-
-    while row < num and col2 < num:
-        arr[row][col2] = BANNED
-        row += 1
-        col2 += 1
-
+        if col1 >= 0:
+            arr[i][col1] = BANNED
+            col1 -= 1
+        if col2 < num:
+            arr[i][col2] = BANNED
+            col2 += 1
     return arr
 
 
 def place(arr, row):
     if row >= num:
         return True
-    for i in range(len(arr[row])):
-        if arr[row][i] == 0:
-            arr = ban(arr, row, i)
-            arr[row][i] = QUEEN
-            return
-            # return place(arr, row+1)
 
-    print('problem', row)
-    return False
+    save = copy.deepcopy(arr)
+
+    for col in range(num):
+        if arr[row][col] == 0:
+
+            ban(arr, row, col)
+            arr[row][col] = QUEEN
+            # print(row, col)
+            # print_arr(arr)
+
+            if place(arr, row+1) != save:
+                return arr
+            else:
+                # print(row, 'rollback')
+                arr = copy.deepcopy(save)
+                # print()
+                # print_arr(arr)
+
+    return save
 
 
 place(array, 0)
-place(array, 1)
-place(array, 2)
-place(array, 3)
 
-end_list = list(range(1, num+1))
-print(end_list)
-for i in array:
-    print(*i, 0)
+# print_arr(array)
+for i in range(num):
+    for j in range(num):
+        if array[i][j] != BANNED:
+            print(i, j)
+
+# print(end_list)
+# for i in array:
+#     print('\t'.join(map(str, i)), '\t', 0)
